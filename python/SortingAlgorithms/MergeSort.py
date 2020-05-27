0ff0728925bc20ffaaee4512b202390789f328a0
99sbr <y13uc295@lnmiit.ac.in>
2020-05-27 20:32:24 +0530

    merge  sort

--- python/SortingAlgorithms/MergeSort.py
+++ python/SortingAlgorithms/MergeSort.py
@@ -1,32 +1,37 @@
-def merge_sort_algorithm(array):
-    if len(array) > 1:
-        n = len(array)
-        L = array[:n//2]
-        R = array[n//2:]
-
-        merge_sort_algorithm(L)
-        merge_sort_algorithm(R)
-
-        i = j = k = 0
-
-        while(i < len(L) and j < len(R)):
-            if L[i] < R[j]:
-                array[k] = L[i]
-                i += 1
-            else:
-                array[k] = R[j]
-                j += 1
-            k += 1
-
-        while(i < len(L)):
+def merge(L, R, array):
+    nL = len(L)
+    nR = len(R)
+    n = len(array)
+    i = j = k = 0
+    while i < nL and j < nR:
+        if L[i] < R[j]:
             array[k] = L[i]
             i += 1
-            k += 1
-
-        while(j < len(R)):
+        else:
             array[k] = R[j]
             j += 1
-            k += 1
+        k += 1
+
+    while i < nL:
+        array[k] = L[i]
+        k += 1
+        i += 1
+    while j < nR:
+        array[k] = R[j]
+        j += 1
+        k += 1
+    return array
+
+
+def merge_sort_algorithm(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        L = array[:mid]
+        R = array[mid:]
+        merge_sort_algorithm(L)
+        merge_sort_algorithm(R)
+        array = merge(L, R, array)
+        return array
 
 
 def printList(arr):
@@ -36,7 +41,7 @@ def printList(arr):
 
 
 if __name__ == '__main__':
-    arr = [12, 11, 13, 5, 6, 7]
+    arr = [12, 11, 13, 8, 7, 6]
     print("Given array is", end="\n")
     printList(arr)
     merge_sort_algorithm(arr)
